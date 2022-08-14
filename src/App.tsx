630e04cecaa5a59359aeec50d68fb11f5494f6e2
Joy Patel <joypatel041994@gmail.com>
2022-08-14 16:08:41 -0400

    Added react navigation stack along with gesture-handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
